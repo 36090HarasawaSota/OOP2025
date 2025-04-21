2241380e9ea30131984d507d5f01faa107e429be
infosys <infosys@C4203>
2025-04-21 09:57:51 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/productSample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace productSample
+{
+    internal class Program {
+       
+        static void Main(string[] args) { 
+        
+         Product karinto = new Product(123, "かりんとう", 180);
+         Product daifuku = new Product(124, "大福", 7000);
+
+          //税込みの価格を表示(かりんとうの税抜き価格は〇〇円です)
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "です");
+        
+         //消費税額の価格
+         Console.WriteLine(karinto.Name + "の消費税額は" + karinto.GetTax() + "円です");
+
+            //税込み価格の表示
+            Console.WriteLine(karinto.Name + "の税込み価格は、" + karinto.GetPriceIncIudingTax() + "円です。");
+
+        //合計金額の表示
+         Console.WriteLine("合計金額は" + (karinto.GetPriceIncIudingTax() + daifuku.GetPriceIncIudingTax()) + "です");   
+
+        }
+    }
+}
---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int start = int.Parse(args[1]);               //int.Parse()=int型に変換
+            int end = int.Parse(args[2]);
+
+            if (args.Length >= 1 && args[0] == "-tom") {      //&&＝かつ
+                PrintFeetToMeterList(start, end);
+            } else {
+                PrintMeterToFeetList(start, end);
+            }
+        }
+
+        //フィートからメートルへの対応表を出力  
+        static void PrintFeetToMeterList(int start int end) {
+            for (int feet = start; feet <= end; feet++) {
+                double meter = MeterToFeet(feet);
+                Console.WriteLine($"{feet}m = {meter:0.0000}ft");
+            }
+        }
+
+        //メートルからメートルへの対応表を出力
+        static void PrintMeterToFeetList(int start int end) {
+            for (int meter = start; meter <= end; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
+        }
+    }
+}
+    }
+}
+
