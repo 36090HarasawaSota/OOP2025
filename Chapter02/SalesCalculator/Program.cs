a8336c6b278100b82b4f54cbc298c2727a311932
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-05-09 15:21:01 +0900

    コンストラクタの引数変更

--- Chapter02/SalesCalculator/Program.cs
+++ Chapter02/SalesCalculator/Program.cs
@@ -4,7 +4,7 @@
         static void Main(string[] args) {
 
            
-            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales(@"data\sales.csv"));
+            SalesCounter sales = new SalesCounter(@"data\sales.csv");
             Dictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string, int> obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
