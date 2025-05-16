911f0a361d501c0396f5dafc4beb753a614293ce
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-05-16 14:02:43 +0900

    p65まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -3,36 +3,27 @@
 namespace Section01 {
     internal class Program {
             
-        public delegate bool Judgement(int value);          // dデリゲートの宣言
-
-        static void Main(string args) { 
+        static void Main(string[] args) { 
            // Console.WriteLine("カウントしたい数字");
            //int num = int.Parse(Console.ReadLine());
            
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, };
 
-            Judgement judge = IsEven;
-            Console.WriteLine(Count(numbers,judge));
+            Console.WriteLine(Count(numbers,delegate(int n) { return n % 2 == 0; }));
             
             }
-            
-            //メソッドへ渡す処理
-            static bool IsEven(int n) {
-            return n % 2 == 0;
-            }
-
-                static int Count(int[] numbers,Judgement judge) {
-                var count = 0;
-
+           
+                static int Count(int[] numbers,Predicate<int> judge) {
+                    var count = 0;
                     foreach (var n in numbers) {
-                        if (judge(n)  == true) {
+                    //引数で受け取ったメソッドを呼び出す   
+                    if (judge(n)  == true) {
                         count++;
                         }
                     }
 
-                return count;
-
-
+                    return count;
                 }
+
     }
 }
