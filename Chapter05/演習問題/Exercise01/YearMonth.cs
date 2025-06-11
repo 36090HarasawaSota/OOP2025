184bf0c0d2261833a58f4a953450add61a5fff67
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-06-11 11:18:40 +0900

    演習8.1.1完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -6,14 +6,22 @@ using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Exercise01 {
+
     //5.1.1
     public record YearMonth (int year, int month){
-        public int Year { get; init; } = year;
-        public int Month { get; init; } = month;
+        public int Year { get; init; } = year;　　//= year 〇〇コンストラクタ　普通のコンストラクタでいちいちメソッドを作らず直接入れられる
+        public int Month { get; init; } = month;  //= month　〇〇コンストラクタ
+
+        //public YearMonth(int year = 0, int month = 0) {
+        //    Year = year;
+        //    Month = month;
+        //}
+
 
         //5.1.2
-        public bool Is21Century => 2001 <= Year && Year <= 2100;
-
+        public bool Is21Century => 2001 <= Year && Year <= 2100; //プロパテイー　右側の条件に当てはまるなら
+                                                                 //型に沿ったものを返す　　今回はboolなのでtrueかfalseを返す  プロパティーは呼び出すとき()を書いたらエラーになる   
+       
         //5.1.3
         public YearMonth AddOneMonth() {
             if (Month < 12) {
---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+
+            Exercise2(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            var numcount = new Dictionary<char, int>();
+            foreach (var item in text.ToUpper().OrderBy(n=> text)) {
+                if('A' <= item && item <= 'Z'){
+                    if (numcount.ContainsKey(item)) {
+                        numcount[item] += 1;
+                    } else {
+                        numcount.Add(item, 1);
+                    }
+                }
+            }
+            foreach (var num in numcount) {
+                Console.WriteLine($"{num.Key}: {num.Value}");
+            }
+        }
+
+
+        private static void Exercise2(string text) {
+         
+        }
+    }
+}
