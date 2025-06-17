48c8b76a2e6388ff0a61bee75bfb3b26dc07060a
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-06-17 11:35:05 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/ScoreCounter.cs
@@ -0,0 +1,46 @@
+﻿namespace Test01 {
+    public class ScoreCounter {
+        private readonly IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+            GetPerStudentScore();
+
+        }
+
+        //メソッドの概要： 
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            //売り上げデータを入れるリストオブジェクトを生成
+            var sales = new List<Student>();
+            //ファイルを一気に読み込み
+            var lines = File.ReadAllLines(filePath);
+            //読み込んだ行数分繰り返し
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                //Saleオブジェクトを生成
+                var sale = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = items[2]
+                };
+                sales.Add(sale);
+            }
+                return sales;
+        }
+      
+        
+
+        //メソッドの概要： 
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new SortedDictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject)) {
+                    dict[score.Subject] += score.Score;
+                } else {
+                    dict[score.Subject] = score.Score;
+                }
+            }
+        return dict;
+        }
+    }
+}
---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+
+        public string Name { get; } = String.Empty;
+        public string Subject { get; } = String.Empty;
+        public int Score { get; }
+
+    }
+}
