bb382230deede1f23ffdeffb29473a01ca0fbda6
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-06-17 12:08:52 +0900

    設問1＿提出

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -9,34 +9,34 @@
         }
 
         //メソッドの概要： 
-        //private static IEnumerable<Student> ReadScore(string filePath) {
-         //   var sales = new List<Student>();
-         //   var lines = File.ReadAllLines(filePath);
-         //
-          //  foreach (var line in lines) {
-          //      var items = line.Split(',');
-          //      var Score = new Student() {
-           //         Name = items[0],
-            //        Subject = items[1],
-            //       Score = items[2]
-            //   };
-            //  sales.Add(Score);
-           // }
-            //    return sales;
-        //}
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            var sales = new List<Student>();
+           var lines = File.ReadAllLines(filePath);
+         
+            foreach (var line in lines) {
+             var items = line.Split(',');
+                var Score = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                   Score = items[2]
+               };
+              sales.Add(Score);
+            }
+                return sales;
+        }
       
         
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new SortedDictionary<string, int>();
-            //foreach (var score in _score) {
-             //   if (dict.ContainsKey(score.Subject)) {
-             //       dict[score.Subject] += score.Score;
-             //   } else {
-             //       dict[score.Subject] = score.Score;
-              //  }
-           // }
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Subject)) {
+                   dict[score.Subject] += score.Score;
+                } else {
+                   dict[score.Subject] = score.Score;
+              }
+            }
         return dict;
         }
     }
