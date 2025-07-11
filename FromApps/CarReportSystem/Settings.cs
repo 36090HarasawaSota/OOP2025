3a8db3f8a4a0dfb0820c7dcce1d8840ebd0dcc5c
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-07-11 13:48:26 +0900

    シングルトン終了

--- FromApps/CarReportSystem/Settings.cs
+++ FromApps/CarReportSystem/Settings.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarReportSystem{
-    public class Settings{
+namespace CarReportSystem {
+    public class Settings {
+
+        private static Settings instance; //自分自身のインスタンスを格納
 
         //設定した色情報を格納
         public int MainFormBackColor { get; set; }
+
+        //コンストラクタ(privateにすることでnewできなくなる）
+        private Settings() { }
+
+        public static Settings getInstance() {
+            if (instance == null) { 
+                instance = new Settings();
+            }
+            return instance;
+        }
     }
 }
