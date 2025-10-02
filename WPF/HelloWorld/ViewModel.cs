55ae2649ef27678ac59db0c716884e853750ed03
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-10-02 15:46:29 +0900

    IsEnable属性

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -12,16 +12,30 @@ namespace HelloWorld{
     class ViewModel : BindableBase {
 
         public ViewModel() {
-            ChangeMessageCommand = new DelegateCommand(
-                ()=> GreetingMessage = "Bye-bye world");
+            ChangeMessageCommand = new DelegateCommand<string>(
+                (par)=> GreetingMessage = par);
         }
 
         private string _greetingMessage = "HelloWorld!";
             public string GreetingMessage {
             get => _greetingMessage;
-            set => SetProperty(ref _greetingMessage, value);
+            set {
+                    if(SetProperty(ref _greetingMessage,value)) {
+                    CanChangeMessage = false;
+                    }
+                }
             }
-            public DelegateCommand ChangeMessageCommand {get;}
+
+        private bool _canChangeMessage = true;
+        public bool CanChangeMessage {
+            get => _canChangeMessage;
+            private set => SetProperty(ref _canChangeMessage,value);
+        }
+
+        public string NewMessage1 { get; } = "Bye-bye";
+        public string NewMessage2 { get; } = "goodbye";
+
+        public DelegateCommand<string> ChangeMessageCommand { get; }
 
     
     }
