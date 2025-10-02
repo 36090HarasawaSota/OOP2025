01f15088d906077ad7b818d4c5035c389e0ea53f
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-10-02 15:00:10 +0900

    ボタンによる文字列の変更

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace HelloWorld{
 
-    class ViewModel :INotifyPropertyChanged {
+    class ViewModel : BindableBase {
 
         public ViewModel() {
             ChangeMessageCommand = new DelegateCommand(
@@ -19,14 +19,10 @@ namespace HelloWorld{
         private string _greetingMessage = "HelloWorld!";
             public string GreetingMessage {
             get => _greetingMessage;
-            set {
-                if (_greetingMessage != value) {
-                    PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(GreetingMessage)));
-                }
+            set => SetProperty(ref _greetingMessage, value);
             }
-        }
             public DelegateCommand ChangeMessageCommand {get;}
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+    
     }
 }
