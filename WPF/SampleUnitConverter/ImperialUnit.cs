91c33be5cf12ea84e5246dd620e513cbe3ae97b7
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-10-09 13:41:08 +0900

    単位変換アプリ完成Prism変更前

---
+++ WPF/SampleUnitConverter/ImperialUnit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitConverter{
+    class ImperialUnit : DistanceUnit {
+        private static List<ImperialUnit> units = new List<ImperialUnit> {
+            new ImperialUnit{Name = "in", Coefficient=1,},
+            new ImperialUnit{Name = "ft", Coefficient=10,},
+            new ImperialUnit{Name = "yd", Coefficient=10 * 100},
+            new ImperialUnit{Name = "ml", Coefficient=10 * 100 * 1000},
+        };
+
+        public static ICollection<ImperialUnit> Units { get => units; }
+
+        //<sumary>
+        //メートル単位からヤード単位に変換します
+        //</sumary>
+        //<param name="unit">変換元の単位</param>
+        //<param name="value">変換する値</param>
+        //<returns>変換した値 </returns>
+
+        public double FromMetricUnit(ImperialUnit unit, double value) {
+            return (value * unit.Coefficient) / 25.4 / Coefficient;
+        }
+
+    }
+}
---
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter{
+    internal class MainWindowViewModel : ViewModel {
+
+        //フィールド
+        private double metricValue;
+        private double imperialValue; 
+
+        //▲で呼ばれるコマンド
+            public ICommand ImperalUnitToMetric { get; private set; }
+        //▼で呼ばれるコマンド
+            public ICommand MetricToImperialunit { get; private set; }
+
+        //上のコンボボックスで選択されている値を入れる箱
+        public MetricUnit CurrentMetricUnit { get; set; }
+        //下のコンボボックスで選択されている値を入れる箱
+        public ImperialUnit CurrentImperialUnit { get; set; }
+
+        //プロパティ
+        public double MetricValue {
+            get => metricValue;
+            set {
+                this.metricValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public double ImperialValue {
+            get => imperialValue;
+            set {
+                this.imperialValue = value;
+                this.OnPropertyChanged();
+
+            }
+        }
+
+        public MainWindowViewModel() {
+
+            CurrentMetricUnit = MetricUnit.Units.First();
+            CurrentImperialUnit = ImperialUnit.Units.First();
+
+            ImperalUnitToMetric = new DelegateCommand(
+                () => MetricValue = CurrentMetricUnit.FromImperialUnit(CurrentMetricUnit, ImperialValue));
+
+            MetricToImperialunit = new DelegateCommand(
+                () => ImperialValue = CurrentImperialUnit.FromMetricUnit(CurrentImperialUnit, MetricValue));
+
+
+        }
+
+
+    }
+}
