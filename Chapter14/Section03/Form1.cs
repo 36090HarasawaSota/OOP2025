28205562323d604f9ccb98359228ed8a74781819
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-10-30 14:52:19 +0900

    コード14.6

---
+++ Chapter14/Section03/Form1.cs
@@ -0,0 +1,19 @@
+namespace Section03 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e) {
+            ToolStripStatusLabel1.Text = "";
+            await Task.Run(()=>DoLongTimeWork());
+            ToolStripStatusLabel1.Text = "èIóπ";
+        }
+
+        private void DoLongTimeWork() {
+            System.Threading.Thread.Sleep(5000);
+        }
+
+
+    }
+}
