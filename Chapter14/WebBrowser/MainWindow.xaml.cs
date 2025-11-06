b9b2467d9c2f490989a1b154d03326c0a86914d2
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-11-06 14:30:18 +0900

    プログレスバーの表示、非表示　完成

--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -20,33 +20,44 @@ public partial class MainWindow : Window
     public MainWindow() {
         InitializeComponent();
         InitializeAsync();
+    }
 
+    public async void InitializeAsync() {
+        await WebView.EnsureCoreWebView2Async(); //非同期にして初期化
+
+        WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationStaring;
+        WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
 
     }
 
-    private async void InitializeAsync() {
-        try {
-            await WebView.EnsureCoreWebView2Async(null);
-            WebView.Source = new Uri("https://www.yahoo.co.jp/");
-        }
-        catch (Exception ex) {
-            MessageBox.Show("初期化エラー: " + ex.Message);
-        }
+    //読み込みを開始したらプログレスバーを表示
+    private void CoreWebView2_NavigationStaring(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+        LoadingBar.Visibility = Visibility.Visible;
+        LoadingBar.IsIndeterminate = true;
     }
 
-
-
+    //読み込みが終了したらプログレスバーを非表示
+    private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e) {
+        LoadingBar.Visibility = Visibility.Collapsed;
+        LoadingBar.IsIndeterminate = true;
+    }
 
     private void BackButton_Click(object sender, RoutedEventArgs e) {
-
+        if (WebView.CanGoBack) {
+            WebView.GoBack();
+        }
     }
 
     private void FowardButton_Click(object sender, RoutedEventArgs e) {
-
+        if (WebView.CanGoForward) {
+            WebView.GoForward();
+        }
     }
 
     private void GoButton_Click(object sender, RoutedEventArgs e) {
-        WebView.Source = new Uri(AddressBar.Text);
+        string url = AddressBar.Text;
+        if (string.IsNullOrWhiteSpace(url)) return;
+        WebView.Source = new Uri(url);
     }
 
 
