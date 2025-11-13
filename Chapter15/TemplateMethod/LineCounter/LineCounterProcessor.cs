1d244094eea8343d0554a6152ca2b28adc41abbe
36090HaraswaSota <oibc36090@ojs.ac.jp>
2025-11-13 13:21:06 +0900

    テンプレートメソッド1完成

---
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace LineCounter {
+    internal class LineCounterProcessor : TextProcessor {
+        private int _count = 0;
+        private string _target = "";
+
+        protected override void Initialize(string fname) {
+            _count = 0;
+            _target = "public";
+        }
+
+        protected override void Execute(string line) {
+            if (line.Contains(_target)) {
+                _count++;
+            }
+        }
+
+        protected override void Terminate() {
+            Console.WriteLine("{0} : {1} 個", _target, _count);
+
+        }
+
+    }
+}
